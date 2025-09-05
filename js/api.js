c2b5f4aaa098fbbe98013d644d750ccf2d3463a8
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-04 21:51:32 -0400

    Add api file

---
+++ js/api.js
@@ -0,0 +1,28 @@
+import { CONFIG } from './config.js'
+
+async function apiCall(city) {
+  try {
+    const { BASE_URL, API_KEY } = CONFIG
+    const response = await fetch(`${BASE_URL}${city}&appid=${API_KEY}`)
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('CITY_NOT_FOUND')
+      } else if (response.status === 401) {
+        throw new Error('INVALID_API_KEY')
+      } else if (response.status === 429) {
+        throw new Error('API_LIMIT_EXCEEDED')
+      } else {
+        throw new Error('API_ERROR')
+      }
+    }
+    return await response.json()
+  } catch (error) {
+    if (error.name === 'TypeError') {
+      throw new Error('NETWORK_ERROR')
+    }
+    throw error
+  }
+}
+
+export default apiCall
